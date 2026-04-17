ab98c29cafa792c97ac69c982bda8460ee667e22
58157667 <58157667@qq.com>
2026-04-17 09:52:49 +0900

    games.jar

---
+++ Games - コピー/src/main/java/com/example/demo/Score.java
@@ -0,0 +1,18 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "scores")
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private int score;
+}
