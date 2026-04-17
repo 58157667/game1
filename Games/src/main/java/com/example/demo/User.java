73f25100aa9cdd7b9650dfdeaac12a3127456314
58157667 <58157667@qq.com>
2026-04-17 23:43:02 +0900

    555

--- Games/src/main/java/com/example/demo/User.java
+++ Games/src/main/java/com/example/demo/User.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 @Entity
--- Games/src/main/java/com/example/demo/UserRepository.java
+++ Games/src/main/java/com/example/demo/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }
