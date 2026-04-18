10743f878b0056c371c6db384869d215576a2815
58157667 <58157667@qq.com>
2026-04-18 13:09:40 +0900

    44

--- Games/src/main/java/com/example/demo/User.java
+++ Games/src/main/java/com/example/demo/User.java
@@ -14,7 +14,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String username;
     private String password;
     private int gold;
